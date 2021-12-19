0b30c4b5b551c2802f1f205d8cc4f0a8b9bd0138
N1ChuanKun <95230582+N1ChuanKun@users.noreply.github.com>
2021-12-20 02:38:09 +0900

    Create 12. vfork.c

---
+++ 12. vfork.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+int a = 0;//자식 프로세스 수량 감시한 변수
+
+int main()
+{
+  pid_t pid;
+  int i = 10;
+  
+  printf("PID(%d): parent a = %d, i = %d\n", getpid(), a, i);
+  
+  pid = vfork();
+  
+  if(pid < 0)
+  {
+    printf("vfork error");
+    exit(1);
+  }
+  else if(pid == 0)
+  {
+    a++;
+    i++;
+    printf("PID(%d): Child a = %d, i = %d\n", getpid(), a, i);
+  }
+  else
+  {
+    printf("PID(%d): parent a = %d, i = %d\n", getpid(), a, i);
+  }
+  
+  printf("PID(%d): parent a = %d, i = %d\n", getpid(), a, i);
+  return 0;
+}
